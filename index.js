5e66318f9c334533d52428d7b581b402b942da74
David Hale <david.hale7@gmail.com>
2018-01-29 10:44:01 -0800

    change albumList to class based component

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Import libraries to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header.js';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create component
-const App = () => {
-  return (
-    <Header />
-  );
-};
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
